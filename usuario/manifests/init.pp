3becb56a16cfb4e33ef941b5b52b911591b79ad6
Tales Santos <talesteixeira@gmail.com>
2016-09-02 19:50:43 -0300

    Setting password

--- usuario/manifests/init.pp
+++ usuario/manifests/init.pp
@@ -4,6 +4,7 @@ user { 'testuser1':
   ensure           => 'present',
   gid              => '1002',
   home             => '/home/testuser',
+  password         => '$6$D1mlVsXd$XWI53M4pGUvPlkWgaZmORGbyFbxQEEGaKXpqvVgvymmP8I956b2fTWfT1O/HUcgfcLhphtjzizmTQdlXse30C/',
   shell            => '/bin/bash',
 }
 }
