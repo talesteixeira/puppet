dd12b465bff6025ba5eb4e835b00f1acd0cf01d3
root <root@ubuntu14.puppet>
2016-09-27 10:53:31 -0400

    Teste

--- usuario/manifests/init.pp
+++ usuario/manifests/init.pp
@@ -1,7 +1,7 @@
 class usuario::cria {
 
 user { 'testuser1':
-  ensure           => 'absent',
+  ensure           => 'present',
   gid              => '1002',
   home             => '/home/testuser',
   password         => '$6$D1mlVsXd$XWI53M4pGUvPlkWgaZmORGbyFbxQEEGaKXpqvVgvymmP8I956b2fTWfT1O/HUcgfcLhphtjzizmTQdlXse30C/',
