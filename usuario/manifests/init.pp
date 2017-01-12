6ead236999648817452cf5a7a0f3e0170a4954e2
Tales Santos <talesteixeira@gmail.com>
2017-01-12 01:02:20 -0200

    Testes CI Jenkins

--- usuario/manifests/init.pp
+++ usuario/manifests/init.pp
@@ -2,9 +2,6 @@ class usuario::cria {
 
 user { 'testuser1':
   ensure           => 'present',
-  gid              => '1002',
-  home             => '/home/testuser',
   password         => '$6$D1mlVsXd$XWI53M4pGUvPlkWgaZmORGbyFbxQEEGaKXpqvVgvymmP8I956b2fTWfT1O/HUcgfcLhphtjzizmTQdlXse30C/',
-  shell            => '/bin/bash',
 }
 }
