679dd897aafadee324f80659fc94d5ff79a1b61e
Tales Santos <tales@equinix.com.br>
2017-01-12 01:20:20 -0200

    Teste 4 Jenkins Openshift

--- usuario/manifests/init.pp
+++ usuario/manifests/init.pp
@@ -3,7 +3,7 @@
 class usuario::cria {
 
 user { 'testuser1':
-  ensure           => 'present',
-  password         => '$6$D1mlVsXd$XWI53M4pGUvPlkWgaZmORGbyFbxQEEGaKXpqvVgvymmP8I956b2fTWfT1O/HUcgfcLhphtjzizmTQdlXse30C/',
+  ensure   => 'present',
+  password => '$6$D1mlVsXd$XWI53M4pGUvPlkWgaZmORGbyFbxQEEGaKXpqvVgvymmP8I956b2fTWfT1O/HUcgfcLhphtjzizmTQdlXse30C/',
 }
 }
